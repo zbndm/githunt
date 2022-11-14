7cebaa1f9c5935618fae521afe9ebc9fced4d637
dmytro zybin <zbndmt@gmail.com>
2022-11-14 11:16:06 +0000

    9

---
+++ build/precache-manifest.8989895c8e3c5b95ffd2a6b5d7b0627e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d045a14a0cf540dc522f65803aa4445b",
+    "url": "/githunt/index.html"
+  },
+  {
+    "revision": "81bef498620d55cfc886",
+    "url": "/githunt/static/css/2.5a632e4b.chunk.css"
+  },
+  {
+    "revision": "bbce7990d89f03d5ba4d",
+    "url": "/githunt/static/css/main.5b277643.chunk.css"
+  },
+  {
+    "revision": "81bef498620d55cfc886",
+    "url": "/githunt/static/js/2.ba5b9ad6.chunk.js"
+  },
+  {
+    "revision": "4dfe0d1c0c4999b3ec5db0050d4eb26c",
+    "url": "/githunt/static/js/2.ba5b9ad6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbce7990d89f03d5ba4d",
+    "url": "/githunt/static/js/main.a12deff8.chunk.js"
+  },
+  {
+    "revision": "2a62761ec38dcfffca38",
+    "url": "/githunt/static/js/runtime-main.32225ef5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/githunt/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/githunt/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/githunt/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/githunt/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/githunt/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
